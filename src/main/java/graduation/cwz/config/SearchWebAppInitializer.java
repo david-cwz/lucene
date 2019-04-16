f4cbe8d0ab5e39868440f9b4faedfc52d6378415
david <david_cwz@qq.com>
2019-04-16 14:30:26 +0800

    david_s 前端模板调试

--- src/main/java/graduation/cwz/config/SearchWebAppInitializer.java
+++ src/main/java/graduation/cwz/config/SearchWebAppInitializer.java
@@ -1,20 +1,20 @@
-package graduation.cwz.config;
-
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-public class SearchWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[] {RootConfig.class};
-    }
-
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[] {WebConfig.class};
-    }
-
-    @Override
-    protected String[] getServletMappings() {
-        return new String[] {"/"};
-    }
-}
+//package graduation.cwz.config;
+//
+//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+//
+//public class SearchWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+//    @Override
+//    protected Class<?>[] getRootConfigClasses() {
+//        return new Class[] {RootConfig.class};
+//    }
+//
+//    @Override
+//    protected Class<?>[] getServletConfigClasses() {
+//        return new Class[] {WebConfig.class};
+//    }
+//
+//    @Override
+//    protected String[] getServletMappings() {
+//        return new String[] {"/"};
+//    }
+//}
--- src/main/java/graduation/cwz/controller/HomeController.java
+++ src/main/java/graduation/cwz/controller/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
 
   @RequestMapping(method = GET)
   public String home() {
-      return "home";
+      return "index";
   }
 
 }
